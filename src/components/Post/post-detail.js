2e46fe5b8f936d52c96e9aa1a8e3b6916ccce0af
Uthaeus <romanlavery@gmail.com>
2023-05-19 16:47:57 -0600

    added styling to post item and post detail

--- src/components/Post/post-detail.js
+++ src/components/Post/post-detail.js
@@ -40,19 +40,40 @@ function PostDetail() {
 
     return (
         <div className="post-detail-container">
-            <img className="post-detail-img" src={postImage} alt={post.content} />
-            <hr />
-            <h2>{post.content}</h2>
-            <p>by {post.user?.username} {createdAgo >= 1 ? `${Math.round(createdAgo)} days` : createdAgo * 24 >= 1 ? `${Math.round(createdAgo * 24)} hours` : createdAgo * 24 * 60 >= 1 ? `${Math.round(createdAgo * 24 * 60)} minutes` : `${Math.round(createdAgo * 24 * 60 * 60)} seconds` } ago.</p>
+            <div className="post-detail-main-wrapper">
+                <img className="post-detail-img" src={postImage} alt={post.content} />
+                <hr />
+                <div className="detail-body-wrapper">
+                    <div className="detail-content-wrapper">
+                        <p>by {post.user?.username} {createdAgo >= 1 ? `${Math.round(createdAgo)} days` : createdAgo * 24 >= 1 ? `${Math.round(createdAgo * 24)} hours` : createdAgo * 24 * 60 >= 1 ? `${Math.round(createdAgo * 24 * 60)} minutes` : `${Math.round(createdAgo * 24 * 60 * 60)} seconds` } ago.</p>
+                        <h2>{post.content}</h2>
+                    </div>
 
-            <div className="post-detail-links-wrapper">
-                {(user?.id === post.user_id || user.role === 'site_admin') && (
-                <>
-                    <Link to={`/posts/${id}/edit`} className="detail-btn edit">Edit Post</Link>
-                    <Link className="detail-btn delete" onClick={deletePostHandler}>Delete Post</Link>
-                </>
-                )}
-                <Link to="/posts" className="post-detail-link">Back to Posts</Link>
+                    <div className="post-detail-links-wrapper">
+                        {(user?.id === post.user_id || user?.role === 'site_admin') && (
+                        <>
+                            <Link to={`/posts/${id}/edit`} className="detail-btn edit"><i className="bi bi-pencil-square"></i></Link>
+                            <Link className="detail-btn delete" onClick={deletePostHandler}><i className="bi bi-trash3"></i></Link>
+                        </>
+                        )}
+                        <Link to="/posts" className="post-detail-link">Back to Posts</Link>
+                    </div>
+                </div>
+
+                <div className="post-detail-comment-form-wrapper">
+                    { user && (
+                        <p className="temp-comment-form"></p>
+                    )}
+                    
+                </div>
+            </div> 
+
+            <div className="post-detail-comments-wrapper">
+            <h3 className="post-detail-comments-title">Comments</h3>
+                <hr />
+                <div className="post-detail-comments">
+
+                </div>
             </div>
         </div>
     );
--- src/components/Post/post-item.js
+++ src/components/Post/post-item.js
@@ -3,10 +3,20 @@ import { Link } from "react-router-dom";
 function PostItem({post}) {
     let postImage = `http://localhost:4000/${post.image?.url}`;
 
+    function shortenContent(content) {
+        if (content.length > 50) {
+            return `${content.slice(0, 50)}...`;
+        }
+        return content + '...';
+    }
+
     return (
-        <Link to={`/posts/${post.id}`} className="post-item-wrapper col">
-            <img src={postImage} alt={post.content} width='200px' height='250px' />
-            <p>{post.user?.username}</p>
+        <Link to={`/posts/${post.id}`} className="post-item-wrapper">
+            <div className="post-item-content-wrapper">
+                <img src={postImage} alt={post.content} width='100%' height='250px' />
+                <p className="post-item-content">{shortenContent(post.content)}</p>
+            </div>
+            <p className="post-item-user">posted by: <span className="post-item-user-span">{post.user?.username}</span></p>
         </Link>
     );
 }
